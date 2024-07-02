85113447edf35b8ed7f3066e33d7dd20eb4428a6
MD IFTAKHAR HOSSAIN RAFI <ihrafi02ctg@gmail.com>
2024-07-03 01:25:13 +0600

    print integar in 2 ways

---
+++ printInt.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+    //print integar value
+    int a=10;
+    printf("value of a is %d \n\n",a);
+
+
+
+    //read and then print integar value
+    int b;
+    printf("Enter the value of b \n");
+    scanf( "%d", &b); //%d takes decimal inputs
+    printf("Value of b is %d \n",b);
+
+
+    return 0;
+}
