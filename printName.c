d87bd442de7c167c03ce2b62af9690419aa1ecac
MD IFTAKHAR HOSSAIN RAFI <ihrafi02ctg@gmail.com>
2024-07-03 00:54:31 +0600

    printName in 3 ways

---
+++ printName.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int  main()
+{
+    //  1. print using printf()
+
+    printf("My Name is: Iftakhar. \n");
+
+
+    //  2. print using scanf()
+
+    char name[20];
+    printf("Enter Name: ");
+
+    //scanf will take input from user
+    scanf("%s", name);
+    printf("Your Name is: %s", name);
+
+
+    //  3. using fgets()
+    /*fgets() will take input from user and store in a string.
+    open 3rd code in another fresh file it will not run in the same file. Added here because of the same topic.*/
+
+    char x[20];
+    printf("Enter Name: ");
+    fgets(x, 20, stdin);
+    printf("Your Name is: %s", x);
+
+
+
+    return 0;
+}
